40133abbba0cf198f9fd38fcaf20adaf08fc0170
Spyros Chatzinikolaou <Spyros@Spyross-Mac-mini.local>
2013-04-30 15:09:57 +0300

    Loukia Commit

---
+++ Panos/Loukia.h
@@ -0,0 +1,13 @@
+//
+//  Loukia.h
+//  Panos
+//
+//  Created by Spyros Chatzinikolaou on 4/30/13.
+//  Copyright (c) 2013 Birdline Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Loukia : NSObject
+
+@end
---
+++ Panos/Loukia.m
@@ -0,0 +1,13 @@
+//
+//  Loukia.m
+//  Panos
+//
+//  Created by Spyros Chatzinikolaou on 4/30/13.
+//  Copyright (c) 2013 Birdline Software. All rights reserved.
+//
+
+#import "Loukia.h"
+
+@implementation Loukia
+
+@end
